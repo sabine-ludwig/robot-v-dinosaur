a8e0a3ee1d1a0a5f194a407b8a36ea7774897226
Sabine Ludwig <sabinemichelle31@gmail.com>
2023-02-17 08:40:02 -0800

    added framework for robot class

---
+++ robot.py
@@ -0,0 +1,13 @@
+class Robot:
+
+    def __init__(self, name):
+        self.name = input("What would you like your robot's name to be? > ")
+        self.health = 100
+        self.active_weapon = "laser"
+        pass
+
+    def attack(self, dinosaur):
+        self.dinosaur = 
+        pass
+
+    
