71555bb719e7f2f5aeb960313dfd6ad49c17bafd
Sabine Ludwig <sabinemichelle31@gmail.com>
2023-02-17 08:36:25 -0800

    added framework for battlefield class

---
+++ battlefield.py
@@ -0,0 +1,18 @@
+class Battlefield:
+
+    def __init__(self):
+        pass
+        self.robot = 
+        self.dinosaur = 
+
+    def run_game(self):
+        pass
+
+    def display_welcome(self):
+        pass
+
+    def battle_phase(self):
+        pass
+
+    def display_winner(self):
+        pass
